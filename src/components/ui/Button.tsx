1df335af8bfbfaf9a1066ea196995c622c416af2
yoosaemsol <yoosaemsol@gmail.com>
2023-12-08 18:27:25 -0500

    feat: add new props to button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,13 +4,48 @@ import styled from 'styled-components';
 interface ButtonProps {
   children: ReactNode;
   onClick?: MouseEventHandler<HTMLButtonElement>;
+  layoutVariant?: 'contained' | 'outlined';
+  variant?: 'primary' | 'accent';
+  size?: 'medium' | 'large';
 }
 
-export default function Button({ children, onClick }: ButtonProps) {
-  return <ButtonComponent onClick={onClick}>{children}</ButtonComponent>;
+interface ButtonStyleProps {
+  $layoutVariant: 'contained' | 'outlined';
+  $variant: 'primary' | 'accent';
+  $size?: 'medium' | 'large';
 }
 
-const ButtonComponent = styled.button`
+export default function Button({
+  children,
+  onClick,
+  layoutVariant = 'contained',
+  variant = 'primary',
+  size = 'medium',
+}: ButtonProps) {
+  return (
+    <ButtonComponent
+      $layoutVariant={layoutVariant}
+      $variant={variant}
+      $size={size}
+      onClick={onClick}
+    >
+      {children}
+    </ButtonComponent>
+  );
+}
+
+const getColorVaribale = (variant: 'primary' | 'accent') => {
+  switch (variant) {
+    case 'primary':
+      return 'var(--color-primary)';
+    case 'accent':
+      return 'var(--color-accent)';
+    default:
+      return '';
+  }
+};
+
+const ButtonComponent = styled.button<ButtonStyleProps>`
   padding: 7px 12px;
   background-color: var(--color-primary);
   border: none;
@@ -23,4 +58,40 @@ const ButtonComponent = styled.button`
   &:hover {
     background-color: var(--color-primary-active);
   }
+
+  ${({ $variant }) =>
+    `
+    background-color: ${getColorVaribale($variant)};
+    border: 1px solid;
+    color : var(--color-light-level0);
+    border-color :${getColorVaribale($variant)};
+
+      &:hover {
+        border-color :  ${getColorVaribale($variant)};
+        background-color : inherit;
+        color: ${getColorVaribale($variant)};
+      }
+  `}
+
+  ${({ $layoutVariant, $variant }) =>
+    $layoutVariant === 'outlined' &&
+    `
+    background-color: inherit;
+    border: 1px solid;
+    border-color: ${getColorVaribale($variant)};
+    color : ${getColorVaribale($variant)};
+
+      &:hover {
+        border-color :  ${getColorVaribale($variant)};
+        background-color: ${getColorVaribale($variant)};
+        color: var(--color-light-level0);
+      }
+  `}
+
+${({ $size }) =>
+    $size === 'large' &&
+    `
+  font-size: 16px;
+  padding: 7px 20px;
+  `}
 `;
